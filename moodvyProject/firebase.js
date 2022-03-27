8f063a6e68cb450011083118717b1c87c486c2f5
IKayZox <77612305+IKayZox@users.noreply.github.com>
2022-03-27 01:33:16 +0100

    Mise en place connexion/inscription

---
+++ moodvyProject/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase/compat'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRHluF0oe41l-NNn0jThMgvWLAu9yUMo0",
+  authDomain: "appmob-d89ec.firebaseapp.com",
+  projectId: "appmob-d89ec",
+  storageBucket: "appmob-d89ec.appspot.com",
+  messagingSenderId: "1014810735025",
+  appId: "1:1014810735025:web:885560d4c0ab48aa9d4d90"
+};
+
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth();
+
+// Initialize Firebase
+export {auth};
